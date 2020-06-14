672c89b6db8ebc7a51758255f704cfe072b5d641
Jan Van den Schilden <jan.vandenschilden@gmail.com>
2020-06-14 18:02:37 +0200

    Added results 2020-03-21.corona

---
+++ results/2020-03-21.corona/cleanDirectory.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+LOCALDIR=$(realpath $(dirname $0))
+
+rm ${LOCALDIR}/*.fasta
+rm ${LOCALDIR}/*.clstr
+rm ${LOCALDIR}/*.list
+rm ${LOCALDIR}/*.tab
+rm ${LOCALDIR}/*.log
+rm ${LOCALDIR}/*.pim
+rm ${LOCALDIR}/*.tmp
+rm -rf __pycache__
